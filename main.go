0a49c58d4e071513f19950660b8f4fb24286f08d
Mirkenan Kazımzade <kenankzmv@gmail.com>
2023-12-01 12:55:51 +0400

    Update main.go

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 	"net/http"
 	"os"
 
-	"github.com/99designs/gqlgen/graphql/handle"
+	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
 )
 
@@ -17,7 +17,7 @@ const defaultPort = "8080"
 func main() {
 	port := os.Getenv("PORT")
 	if port == "" {
-		port := defaultPort
+		port = defaultPort
 	}
 
 	db := store.NewStore()
